fe66c50b0bcc25e5f9d0a194494bf78616ac42a9
Seba Sanchez <sebastian@tcit.cl>
2022-09-03 18:27:49 -0400

    chore: added react-navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+  );
+}
+
+function ReviewScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Review Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Review" component={ReviewScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
