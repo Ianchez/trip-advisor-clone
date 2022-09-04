c76516a256d27af55ab6e3c06f5240e022c5dccc
Seba Sanchez <sebastian@tcit.cl>
2022-09-03 20:41:42 -0400

    chore: added style to bottom tab-navigator and separated screens

--- App.tsx
+++ App.tsx
@@ -1,45 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator, BottomTabNavigationOptions } from '@react-navigation/bottom-tabs';
 
-function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {
+  ExploreScreen,
+  SearchScreen,
+  PlanScreen,
+  ReviewScreen
+} from './src/screens';
 
-function ReviewScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Review Screen</Text>
-    </View>
-  );
-}
+const getGeneralOptions = ({ route }): BottomTabNavigationOptions  => ({
+  headerShown: false,
+  tabBarIcon: ({ focused }) => {
+    let iconName;
+
+    if (route.name === 'Explore') {
+      iconName = 'home';
+    } else if (route.name === 'Search') {
+      iconName = 'search';
+    } else if (route.name === 'Plan') {
+      iconName = 'favorite-border';
+    } else if (route.name === 'Review') {
+      iconName = 'edit';
+    }
+
+    return <Icon name={iconName} size={26} color={focused ? 'black' : 'gray'} />;
+  },
+  tabBarActiveTintColor: 'black',
+  tabBarInactiveTintColor: 'gray',
+});
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-        <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Navigator initialRouteName="Explore" screenOptions={getGeneralOptions}>
+        <Tab.Screen name="Explore" component={ExploreScreen} />
+        <Tab.Screen name="Search" component={SearchScreen} />
+        <Tab.Screen name="Plan" component={PlanScreen} />
         <Tab.Screen name="Review" component={ReviewScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/index.js
@@ -0,0 +1,11 @@
+import PlanScreen from "./PlanScreen";
+import ExploreScreen from "./ExploreScreen";
+import SearchScreen from "./SearchScreen";
+import ReviewScreen from "./ReviewScreen";
+
+export {
+  PlanScreen,
+  ExploreScreen,
+  SearchScreen,
+  ReviewScreen,
+};
