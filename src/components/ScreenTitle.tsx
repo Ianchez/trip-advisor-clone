a7d190785adc345cd8f339e2294ab7b04f86daea
Seba Sanchez <sebastian@tcit.cl>
2022-09-06 00:04:53 -0400

    chore: ScreenTitle component

--- src/components/ScreenTitle.tsx
+++ src/components/ScreenTitle.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, StyleProp, Text, View, ViewStyle } from 'react-native';
 
 type Props = {
   title: string;
+  style?: StyleProp<ViewStyle>;
 }
 
-const ScreenTitle: React.FC<Props> = ({ title }) => {
+const ScreenTitle: React.FC<Props> = ({ title, style: propStyle }) => {
   return (
-    <View style={styles.container}>
+    <View style={[ styles.container, propStyle ]}>
       <Text style={styles.title}>
         {title}
       </Text>
