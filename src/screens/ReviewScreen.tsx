8cd923592a4ab7e1b87e4cb226181103b9bc41e1
Seba Sanchez <sebastian@tcit.cl>
2022-09-06 02:31:42 -0400

    chore: WIP Review Screen structure

--- src/screens/ReviewScreen.tsx
+++ src/screens/ReviewScreen.tsx
@@ -1,13 +1,109 @@
-import { View } from 'react-native';
-import ScreenTitle from '../components/ScreenTitle';
+import { Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { HorizontalPillList, PillButton, ScreenTitle } from '../components/';
+
 import screenStyles from '../styles/screen';
 
 const ReviewScreen = () => {
   return (
-    <View style={screenStyles.container}>
-      <ScreenTitle title='Review'/>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        decelerationRate={0.3}
+        style={styles.scrollContainer}
+      >
+        <View style={styles.paddingHorizontal}>
+          <ScreenTitle title='Review'/>
+          <View style={styles.separation}/>
+
+          <Text style={styles.noReviewsTitle}>No reviews yet</Text>
+          <Text>You have not written any reviews yet, get started!</Text>
+          <HorizontalPillList
+            pillStyle={{ borderColor: 'black' }}
+            itemList={[
+              'Write a review',
+              'Upload a photo'
+            ]}
+          />
+        </View>
+
+        <View style={styles.separation}/>
+        <View style={[ styles.writeUsAReviewContainer, styles.paddingHorizontal ]}>
+          <View style={styles.writeUsAReview}>
+            <Text style={[ styles.weWantText, styles.white ]}>We want you to write us a review</Text>
+            <Text style={[ styles.white ]}>Because who else would we turn to travel advice?</Text>
+            <View style={styles.separation}/>
+            <PillButton text='What to know' pillStyle={styles.whatToKnowButton}/>
+          </View>
+        </View>
+
+        <View style={styles.doubleSeparation}/>
+        <View style={[ styles.paddingHorizontal, styles.addAPlaceSection ]}>
+          <Text style={{ fontWeight: '500' }}>Is Tripadvisor missing a place?</Text>
+          <Text>Tell us about it so we can improve what we show</Text>
+          <View style={styles.separation}/>
+          <PillButton text='Add a place' pillStyle={styles.addAPlaceButton}/>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  scrollContainer: {
+    flex: 1,
+    paddingVertical: 16,
+  },
+  paddingHorizontal: {
+    paddingHorizontal: 16,
+  },
+  separation: {
+    marginBottom: 20,
+  },
+  doubleSeparation: {
+    marginBottom: 40,
+  },
+  noReviewsTitle: {
+    ...screenStyles.subSectionText,
+    marginBottom: 6,
+  },
+  writeUsAReviewContainer: {
+    backgroundColor: 'black',
+    width: '100%',
+    height: Dimensions.get('window').width,
+    paddingVertical: 26,
+  },
+  writeUsAReview: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'flex-end',
+  },
+  weWantText: {
+    fontSize: 32,
+    fontWeight: '700',
+    marginBottom: 12,
+    lineHeight: 38,
+  },
+  addAPlaceSection: {
+    alignItems: 'center',
+  },
+  whatToKnowButton: {
+    backgroundColor: 'white',
+    width: 114,
+  },
+  addAPlaceButton: {
+    borderColor: 'black',
+    width: '100%',
+    alignItems: 'center'
+  },
+  white: {
+    color: 'white'
+  },
+}); 
 
 export default ReviewScreen;
