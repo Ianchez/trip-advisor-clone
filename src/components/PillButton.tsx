772c30c938856f5bb9733b58107bc985b5f957c2
Seba Sanchez <sebastian@tcit.cl>
2022-09-06 02:28:12 -0400

    chore: PillButton component

---
+++ src/components/PillButton.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { GestureResponderEvent, StyleSheet, StyleProp, TouchableOpacity, Text, ViewStyle, TextStyle } from 'react-native';
+
+type Props = {
+  text: string;
+  pillStyle?: StyleProp<ViewStyle>;
+  textStyle?: StyleProp<TextStyle>;
+  onPress?: (event: GestureResponderEvent) => void;
+}
+
+const PillButton: React.FC<Props> = ({ text, pillStyle, textStyle, onPress }) => {
+  return (
+    <TouchableOpacity style={[ styles.pill, pillStyle ]} onPress={onPress}>
+      <Text style={[ styles.pillText, textStyle ]}>{text}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  pill: {
+    height: 40,
+    justifyContent: 'center',
+    borderWidth: 2,
+    borderRadius: 20,
+    borderColor: '#e9e9e9',
+    paddingHorizontal: 16,
+  },
+  pillText: {
+    fontSize: 12,
+    fontWeight: '500',
+    position: 'relative',
+    bottom: 1,
+  },
+});
+
+export default PillButton;
