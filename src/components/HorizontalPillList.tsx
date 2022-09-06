3fad53a7a51a2c6774cd50bde563c3f9fb1a038b
Seba Sanchez <sebastian@tcit.cl>
2022-09-06 02:30:12 -0400

    refactor: HorizontalPillList using PillButton component

--- src/components/HorizontalPillList.tsx
+++ src/components/HorizontalPillList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ScrollView, StyleSheet, StyleProp, Text, TouchableOpacity, View, ViewStyle, TextStyle } from 'react-native';
+import PillButton from './PillButton';
 
 type Props = {
   itemList: string[],
@@ -18,9 +19,7 @@ const HorizontalPillList: React.FC<Props> = ({ itemList, containerStyle, pillSty
     >
       {itemList.map((item, index) =>
         <>
-          <TouchableOpacity style={[ styles.pill, pillStyle ]}>
-            <Text style={[ styles.pillText, textStyle ]}>{item}</Text>
-          </TouchableOpacity>
+          <PillButton text={item} pillStyle={pillStyle} textStyle={textStyle}/>
           {index !== (itemList.length - 1) && // Avoid space at the end
             <View style={styles.spaceBetween}/>
           }
@@ -34,20 +33,6 @@ const styles = StyleSheet.create({
   pillsContainer: {
     flexDirection: 'row',
     paddingVertical: 12,
-  },
-  pill: {
-    height: 40,
-    justifyContent: 'center',
-    borderWidth: 2,
-    borderRadius: 20,
-    borderColor: '#e9e9e9',
-    paddingHorizontal: 16,
-  },
-  pillText: {
-    fontSize: 12,
-    fontWeight: '500',
-    position: 'relative',
-    bottom: 1,
   },
   spaceBetween: {
     width: 6,
