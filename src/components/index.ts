9f6483cbb628d45ad55d016a928cc496e6ad4ce4
Seba Sanchez <sebastian@tcit.cl>
2022-09-06 02:31:02 -0400

    fix: PillButton import

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,13 @@
 import HorizontalPillList from "./HorizontalPillList";
 import InputField from "./InputField";
+import PillButton from "./PillButton";
 import RatingCircles from "./RatingCircles";
 import ScreenTitle from "./ScreenTitle";
 
 export {
   HorizontalPillList,
   InputField,
+  PillButton,
   RatingCircles,
   ScreenTitle,
 };
