9f0a28b817c1a9aaf1f1829f3f42bc47c5356f1e
Seba Sanchez <sebastian@tcit.cl>
2022-09-06 16:07:56 -0400

    chore: created PlacesRecentSearchList section

---
+++ src/components/sections/PlacesRecentSeachList.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Place } from '../../models/dataTypes';
+import { IRecentSearch } from '../../models/searchTypes';
+
+
+interface Props {
+  resentSearches: IRecentSearch[],
+  places: Place[];
+}
+
+const PlacesRecentSearchList: React.FC<Props> = ({ resentSearches, places }) => {
+  return (
+    <TouchableOpacity>
+      <View style={styles.doubleSeparation}/>
+      <Text style={styles.recentSubTitle}>RECENT</Text>
+      {resentSearches.reverse().map((recent, index) => {
+        const place = places.find(place => place.id === recent.placeId);
+        return (
+          <View key={`recent-search-list-item-${index}`} style={styles.recentListItem}>
+            <Image source={{ uri: place?.imgURI || '' }} style={styles.recentImage} />
+            <View>
+              <Text>{recent.searchString}</Text>
+              {place?.location &&
+                <Text style={styles.placeLocation}>{place.location}</Text>
+              }
+            </View>
+          </View>
+        );
+      })}
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  doubleSeparation: {
+    marginBottom: 40,
+  },
+  recentSubTitle: { fontSize: 11, fontWeight: '500', marginBottom: 20 },
+  recentListItem: { flexDirection: 'row', height: 60, borderTopWidth: 1, borderTopColor: 'lightgray', paddingVertical: 10 },
+  recentImage: { height: 40, width: 40, backgroundColor: 'black', marginRight: 16 },
+  placeLocation: {
+    fontSize: 12,
+  }
+});
+
+export default PlacesRecentSearchList;
