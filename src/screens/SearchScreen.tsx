ff6a83c66b0ff90fde3dff626aca8ded2954fc6d
Seba Sanchez <sebastian@tcit.cl>
2022-09-06 01:06:14 -0400

    chore: WIP search screen with destination section and scrollable

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import HorizontalPillList from '../components/HorizontalPillList';
 import InputField from '../components/InputField';
 import ScreenTitle from '../components/ScreenTitle';
 import PlacesSection from '../components/sections/PlacesSection';
+import DestinationsSection from '../components/sections/DestinationsSection';
 import screenStyles from '../styles/screen';
 
 const PLACES_INITIAL_STATE = [
@@ -50,25 +52,44 @@ const SearchScreen = () => {
   const [places, setPlaces] = useState<any[]>(PLACES_INITIAL_STATE);
 
   return (
-    <View style={screenStyles.container}>
+    <SafeAreaView style={screenStyles.container}>
       <ScreenTitle title='Search' style={styles.separation}/>
-      <InputField placeholder='Where to?' icon={'search'} style={styles.separation}/>
-
-      {Boolean(resentSearches.length) && [
-        <Text style={screenStyles.subSectionText}>Your recent searches</Text>,
-        <HorizontalPillList itemList={resentSearches}/>
-      ]}
-
+      <InputField placeholder='Where to?' icon={'search'} />
       <View style={styles.separation}/>
 
-      <PlacesSection placesList={places} sectionTitle="Attractions nearby"/>
-    </View>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        decelerationRate={0.3}
+        style={styles.scrollContainer}
+      >
+        {Boolean(resentSearches.length) && [
+          <Text style={screenStyles.subSectionText}>Your recent searches</Text>,
+          <HorizontalPillList itemList={resentSearches}/>
+        ]}
+
+        <View style={styles.separation}/>
+        <PlacesSection placesList={places} sectionTitle='Attractions nearby'/>
+        <View style={styles.doubleSeparation}/>
+        <PlacesSection placesList={places} sectionTitle='Bars & Pubs nearby'/>
+        <View style={styles.doubleSeparation}/>
+        <PlacesSection placesList={places} sectionTitle='Weekends getaway'/>
+        <View style={styles.doubleSeparation}/>
+        <DestinationsSection destinationsList={places} sectionTitle='Destinations travelers love' />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flex: 1,
+    paddingVertical: 15,
+  },
   separation: {
-    marginBottom: 20
+    marginBottom: 20,
+  },
+  doubleSeparation: {
+    marginBottom: 40,
   },
 }); 
 
--- src/styles/screen.ts
+++ src/styles/screen.ts
@@ -1,10 +1,12 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, StatusBar } from 'react-native';
 
 const screenStyles = StyleSheet.create({
   container: {
+    flex: 1,
     flexDirection: 'column',
     backgroundColor: '#fff',
-    padding: 15,
+    paddingHorizontal: 15,
+    paddingTop: StatusBar.currentHeight || 0 + 15,
   },
   subSectionText: {
     fontSize: 16,
