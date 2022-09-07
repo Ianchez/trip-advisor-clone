b68992aa4f2f66ee49ca4a2bc02ea2148f30d576
Seba Sanchez <sebastian@tcit.cl>
2022-09-06 21:10:06 -0400

    refactor: SearchScreen moved out a lot of sections

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,10 +1,8 @@
 import { useState } from 'react';
-import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { HorizontalPillList, InputField, ScreenTitle } from '../components/';
-import { PlacesSection, DestinationsSection, PlacesRecentSearchList, PlacesSearchResultList } from '../components/sections';
-
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import { HorizontalPillList, ScreenTitle } from '../components/';
+import { PlacesSection, DestinationsSection, SearchSection } from '../components/sections';
 
 import screenStyles from '../styles/screen';
 
@@ -12,12 +10,11 @@ import { PLACES_INITIAL_STATE } from '../data/places';
 import { IRecentSearch } from '../models/searchTypes';
 import { Place } from '../models/dataTypes';
 
-const SearchScreen = () => {
-  const [searchValue, setSearchValue] = useState('');
+const SearchScreen = ({ navigation }) => {
   const [resentSearches, setRecentSearches] = useState<IRecentSearch[]>([
-    // { placeId: 8, searchString: 'New York City'},
-    // { placeId: null, searchString: 'Things to do in San Pedro de Atacama'},
-    // { placeId: 8, searchString: 'Restaurants in San Jose del Maipo'},
+    { placeId: 8, searchString: 'New York City'},
+    { placeId: null, searchString: 'Things to do in San Pedro de Atacama'},
+    { placeId: 8, searchString: 'Restaurants in San Jose del Maipo'},
   ]);
   const [places, setPlaces] = useState<Place[]>(PLACES_INITIAL_STATE);
   const [searchSection, setSearchSection] = useState(false);
@@ -27,37 +24,23 @@ const SearchScreen = () => {
   const getaways = places.filter(place => place.type === 'getaway');
   const destinations = places.filter(place => place.type === 'destination');
 
-  const onChangeSearchHandler = (value: string) => setSearchValue(value);
-  const onFocusSearchHandler = () => setSearchSection(true);
+  const setSearchSectionActiveHandler = (active: boolean) => setSearchSection(active);
+
+  const onPressItemHandler = (place: Place) => {
+    navigation.navigate('PlaceDetail', { place });
+  }
 
   return (
     <SafeAreaView style={screenStyles.container}>
       {!searchSection && 
         <ScreenTitle title='Search' style={styles.separation}/>
       }
-      <View style={[ styles.searchHeader, searchSection && styles.borderBottomGray ]}>
-        {searchSection &&
-          <TouchableOpacity style={styles.seachHeaderBack} onPress={() => setSearchSection(false)}>
-            <Icon name={'chevron-left'} size={24} color="black" />
-          </TouchableOpacity>
-        }
-        <View style={{ flex: 10 }}>
-          <InputField
-            placeholder='Where to?'
-            icon={'search'}
-            value={searchValue}
-            onChange={onChangeSearchHandler}
-            onFocus={onFocusSearchHandler}
-          />
-        </View>
-      </View>
-
-      {searchSection && !searchValue &&
-        <PlacesRecentSearchList resentSearches={resentSearches} places={places}/>
-      }
-      {searchSection && searchValue &&
-        <PlacesSearchResultList placesList={places} searchValue={searchValue} />
-      }
+      <SearchSection
+        active={searchSection}
+        setActive={setSearchSectionActiveHandler}
+        places={places}
+        resentSearches={resentSearches}
+      />
 
       {!searchSection && <>
         <ScrollView
@@ -66,16 +49,16 @@ const SearchScreen = () => {
           style={styles.scrollContainer}
         >
           {Boolean(resentSearches.length) && <>
-            <Text style={screenStyles.subSectionText}>Your recent searches</Text>,
+            <Text style={screenStyles.subSectionText}>Your recent searches</Text>
             <HorizontalPillList itemList={resentSearches.map(s => s.searchString)} pillStyle={styles.recentSearchPills}/>
           </>}
 
           <View style={styles.separation}/>
-          <PlacesSection placesList={attractions} sectionTitle='Attractions nearby'/>
+          <PlacesSection placesList={attractions} sectionTitle='Attractions nearby' onPressItem={onPressItemHandler}/>
           <View style={styles.doubleSeparation}/>
-          <PlacesSection placesList={barsPubs} sectionTitle='Bars & Pubs nearby'/>
+          <PlacesSection placesList={barsPubs} sectionTitle='Bars & Pubs nearby' onPressItem={onPressItemHandler}/>
           <View style={styles.doubleSeparation}/>
-          <PlacesSection placesList={getaways} sectionTitle='Weekends getaway'/>
+          <PlacesSection placesList={getaways} sectionTitle='Weekends getaway' onPressItem={onPressItemHandler}/>
           <View style={styles.doubleSeparation}/>
           <DestinationsSection destinationsList={destinations} sectionTitle='Destinations travelers love' />
         </ScrollView>
@@ -99,22 +82,7 @@ const styles = StyleSheet.create({
     height: 54,
     borderRadius: 28,
   },
-  searchHeader: {
-    flexDirection: 'row',
-    width: '100%',
-    paddingBottom: 16,
-    alignItems: 'center',
-  },
-  borderBottomGray: {
-    borderBottomWidth: 1,
-    borderBottomColor: 'lightgray',
-  },
-  seachHeaderBack: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 40,
-  }
+
 }); 
 
 export default SearchScreen;
